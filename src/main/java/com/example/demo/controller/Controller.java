9091874bb07e5ce6da9458d103c34b3e4e22d5fa
kaif momin <kaif.momin@gmail.com>
2025-03-07 23:33:58 +0530

    this is first commit for render not connected yet

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String sample(){
+
+        return " hii from Spring boot app ...";
+    }
+
+}
