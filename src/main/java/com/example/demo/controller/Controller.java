ecd56679f7920a9abc65f190470bd3c0c9457928
kaif momin <mominkaif858@gmail.com>
2025-04-06 10:26:50 +0530

    for tyime

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -3,13 +3,16 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class Controller {
 
     @GetMapping("/")
     public String sample(){
 
-        return " hii from Spring boot app ...";
+        return " hii from Spring boot app ..." + LocalDateTime.now();
     }
 
 }
